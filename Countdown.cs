b551c2c9247aca7f267ac61c1a3a7ea260db0437
timwking1 <timwking1@gmail.com>
2024-01-03 15:44:03 -0500

    Initial commit

---
+++ Countdown.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Timers;
+
+namespace xmascd
+{
+    public class Countdown
+    {
+        public TimeSpan tCountdownTimeRemaining;
+        public DateTime dCurrentDate;
+        public DateTime dEndDate;
+        private TimeSpan tTimerDuration;
+
+        public delegate void CountdownTickEventHandler(object sender, EventArgs e);
+        public event CountdownTickEventHandler eCountdownTick;
+
+        public Countdown(DateTime thisCurrentDate, DateTime thisEndDate, int thisTimerDuration)
+        {
+            dCurrentDate = thisCurrentDate;
+            dEndDate = dNextAnnualOccurenceOfEndDate(dCurrentDate, thisEndDate);
+            tTimerDuration = TimeSpan.FromMilliseconds(thisTimerDuration);
+            Timer timer = new Timer(thisTimerDuration); //Creates the Timer instance...
+            timer.Elapsed += CountdownTick;
+            timer.AutoReset = true;
+            timer.Start();
+        }
+        public void CountdownTick(object sender, ElapsedEventArgs e)
+        {
+            dCurrentDate = dCurrentDate.Add(tTimerDuration);
+            tCountdownTimeRemaining = tCalculateRemainingTime(dCurrentDate, dEndDate);
+            eCountdownTick?.Invoke(this, EventArgs.Empty); //Raise event.
+        }
+        TimeSpan tCalculateRemainingTime(DateTime dStartDate, DateTime dEndDate)
+        {
+            if (dEndDate < dStartDate)
+            {
+                throw new ArgumentException("dEndDate must occur after dStartDate."); //Throws an exception if an invalid EndDate is defined.
+            }
+            else
+            {
+                return dEndDate - dStartDate; //Gets the time between the two.
+            }
+        }
+        DateTime dNextAnnualOccurenceOfEndDate(DateTime dStartDate, DateTime dEndDate)
+        {
+            // Adjust endDate to the next occurrence in the current or future years
+            DateTime nextOccurrence = new DateTime(
+                dStartDate.Year,
+                dEndDate.Month,
+                dEndDate.Day,
+                dEndDate.Hour,
+                dEndDate.Minute,
+                dEndDate.Second
+                );
+
+            if (dStartDate > nextOccurrence)
+            {
+                // If the adjusted date is in the past, move to the next year
+                nextOccurrence = nextOccurrence.AddYears(1);
+            }
+            return nextOccurrence;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows;
+using System.Windows.Input;
+
+namespace xmascd
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class ChristmasCountdown : Window
+    {
+        const int christmasMonth = 12;
+        const int christmasDay = 25; //Our Magic Numbers for Christmas day :)
+        private Countdown countdown; //Countdown instance
+
+        public ChristmasCountdown()
+        {
+            InitializeComponent();
+
+            DateTime dCurrentDate = DateTime.Now;
+            DateTime dChristmasDate = new DateTime(dCurrentDate.Year, christmasMonth, christmasDay, 0, 0, 0); //Convert our constants to DateTime format with correct year
+
+            countdown = new Countdown(dCurrentDate, dChristmasDate, 1000); //Create our countdown instance
+            countdown.eCountdownTick += UpdateUI; //Subscribe the UpdateUI function to the Countdown tick event
+        }
+        public void UpdateUI(object sender, EventArgs e)
+        {
+            Dispatcher.Invoke(() => //Thread dispatcher needed as countdown tick event is on another thread.
+            {
+                dayDisplay.Text = countdown.tCountdownTimeRemaining.Days.ToString() + " Days";
+                hourDisplay.Text = countdown.tCountdownTimeRemaining.Hours.ToString() + " Hours";
+                minuteDisplay.Text = countdown.tCountdownTimeRemaining.Minutes.ToString() + " Minutes";
+                secondDisplay.Text = countdown.tCountdownTimeRemaining.Seconds.ToString() + " Seconds";
+            });
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e) //Window Dragging
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+        private void ContextMenuClose_Click(object sender, RoutedEventArgs e) //Context Menu
+        {
+            this.Close();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e) //X Button
+        {
+            this.Close();
+        }
+    }
+}
